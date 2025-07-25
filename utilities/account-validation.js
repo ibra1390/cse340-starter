93e842f46dfafa634c4c3b336e6a92fbad8f49b6
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-07-24 20:21:53 -0600

    email already exists validation

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,6 +1,8 @@
 const utilities = require(".")
-  const { body, validationResult } = require("express-validator")
-  const validate = {}
+const accountModel = require("../models/account-model")
+const { body, validationResult } = require("express-validator")
+const validate = {}
+
 
 /*  **********************************
   *  Registration Data Validation Rules
@@ -23,14 +25,18 @@ const utilities = require(".")
         .isLength({ min: 2 })
         .withMessage("Please provide a last name."), // on error this message is sent.
   
-      // valid email is required and cannot already exist in the DB
+      // valid email is required and cannot already exist in the database
       body("account_email")
-      .trim()
-      .escape()
-      .notEmpty()
-      .isEmail()
-      .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+        .trim()
+        .isEmail()
+        .normalizeEmail() // refer to validator.js docs
+        .withMessage("A valid email is required.")
+        .custom(async (account_email) => {
+            const emailExists = await accountModel.checkExistingEmail(account_email)
+            if (emailExists){
+                throw new Error("Email exists. Please log in or use different email")
+            }
+        }),
   
       // password is required and must be strong password
       body("account_password")
@@ -46,7 +52,6 @@ const utilities = require(".")
         .withMessage("Password does not meet requirements."),
     ]
   }
-
 
 /* ******************************
  * Check data and return errors or continue to registration
