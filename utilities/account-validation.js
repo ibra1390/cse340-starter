0c257c6805f382f2f9bf8be3ea4639a2c900d44b
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-07-24 20:53:31 -0600

    login validation exercise

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -75,4 +75,44 @@ validate.checkRegData = async (req, res, next) => {
   next()
 }
 
+/* **********************************
+ *  Login Data Validation Rules
+ *  ********************************** */
+validate.loginRules = () => {
+  return [
+    // email must be valid
+    body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("A valid email is required."),
+
+    // password must not be empty
+    body("account_password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required.")
+  ]
+}
+
+/* ******************************
+ * Check login data
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  const { account_email } = req.body
+  let errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      errors,
+      title: "Login",
+      nav,
+      account_email
+    })
+    return
+  }
+  next()
+}
+
+
 module.exports = validate
